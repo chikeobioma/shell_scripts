bde1527e37bd0d1e45468f28bde2d5c3c3e4cdfa
Stephen Obioma <stephenobioma@yahoo.com>
2018-08-01 14:45:15 -0500

    Delete diskspace.sh

--- diskspace.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Please enter the name of the host with diskspace issue"
-ACTION='hostname ; df -hl ; sudo -S <<< 2018Birchwood /usr/sbin/logrotate -vf /etc/logrotate.conf ; df -hl ; sudo -S <<< 9009Bbbs$ find /var/lib/puppet/clientbucket -mtime +2 |xargs sudo rm {} \; ; df -hl'
-read hostname
-sshpass -f password.txt ssh -t $hostname $ACTION
-

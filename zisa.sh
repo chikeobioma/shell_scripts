ffd1849588680333b1e86ea792b8e07d339313a7
Stephen Chike Obioma <stephenobioma@yahoo.com>
2019-07-18 08:19:54 -0500

    Update zisa.sh

--- zisa.sh
+++ zisa.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 hostname=$(hostname)
 
-var=$(sudo -iu dotomi zisactl pid < /home/nosmonaliev/passwd.txt | awk '{print $4}') 
+var=$(sudo -iu dotomi zisactl pid <<< /home/chiobi/password.txt | awk '{print $4}') 
 if [ "$var" == "not running" ]
 then
              echo "Zisa is down and will be to restarted."
-sudo -iu dotomi zisactl start < /home/nosmonaliev/passwd.txt 
+sudo -iu dotomi zisactl start <<< /home/chiobi/password.txt
 sleep 20s
-sudo -iu dotomi zisactl status < /home/nosmonaliev/passwd.txt  
-var=$(sudo -iu dotomi zisactl status  < /home/nosmonaliev/passwd.txt | awk '{print $4}')  
+sudo -iu dotomi zisactl status <<< /home/chiobi/password.txt  
+var=$(sudo -iu dotomi zisactl status  <<< /home/chiobi/password.txt | awk '{print $4}')  
 if [ "$var" == "running" ] 
              then
                              echo "Zisa is running."
